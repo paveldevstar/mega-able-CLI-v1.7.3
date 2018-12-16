b1d482a13aac083baf0e41ad2f33bdd1e903659b
paveldevstar <paveldevstar@gmail.com>
2018-12-16 09:18:22 -0500

    megaable angular5 admin template

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('mega-able-v1.7.2 App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
---
+++ src/app/layout/admin/title/title.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {Title} from '@angular/platform-browser';
+import {Router, NavigationEnd, ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-title',
+  template: '<span></span>'
+})
+export class TitleComponent {
+  constructor(private router: Router, private route: ActivatedRoute, private titleService: Title) {
+    this.router.events
+      .filter(event => event instanceof NavigationEnd)
+      .subscribe(() => {
+        let currentRoute = this.route.root;
+        let title = '';
+        do {
+          const childrenRoutes = currentRoute.children;
+          currentRoute = null;
+          childrenRoutes.forEach(routes => {
+            if (routes.outlet === 'primary') {
+              title = routes.snapshot.data.title;
+              currentRoute = routes;
+            }
+          });
+        } while (currentRoute);
+        if (title !== undefined ) {
+          this.titleService.setTitle(title + ' | Mega Able Angular 5+');
+        }
+      });
+  }
+}
